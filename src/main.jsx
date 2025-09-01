a8d0c876e0f259734b7e0a680cdb6a2597d17f33
pie-recruitment <sarah.wixon@pie-recruitment.com>
2025-09-01 21:02:17 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import IncentiveDashboard from "./IncentiveDashboard.jsx";
-export default function App() {
-  return <IncentiveDashboard />;
-}
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
